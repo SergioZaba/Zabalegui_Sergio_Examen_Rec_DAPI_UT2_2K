09fff759ea4485a82a27166aa475d576ed5cc967
SergioZaba <113203945+SergioZaba@users.noreply.github.com>
2023-01-11 19:21:22 +0100

    Recuperacion Terminada

--- Ejercicio 1.py
+++ Ejercicio 1.py
@@ -12,34 +12,56 @@ countries_dict = {"30": "Grecia", "33": "Francia", "34": "España", "351": "Port
                   "49": "Alemania", "7": "Rusia"}
 
 def check_username(nom):
-    """Esta funcion devuelve una cadena con la primera letra de cada palabra en mayuscula"""
+    """Esta funcion devuelve una cadena con la primera letra de cada palabra en mayuscula.
+            PARAMTROS:
+                    -nom: Nombre en minusculas que tenemos que pasar las primeras letras.
+            Salida: Nombre con la primera letra en mayuscula."""
 
     return nom.title()
 
 def check_nif(dni):
     """Esta funcion corrige la letra del DNI introducido realizando el resto de todos los numeros del DNI entre 23
     PARAMETROS:
-                dni=Es el dni introducido por el usuario, después se corregirá"""
+                dni= Es el dni introducido por el usuario, después se corregirá
+                resto= resultado del cociente
+                final= la letra correspondiente a ese cociente"""
     dni_sin_letra = dni[0:8]
     resto = int(dni_sin_letra) % 23
     final = dni_sin_letra + nif_dict[str(resto)]
     return str(final)
 
 def check_phone(num):
+    """Esta es una funcion que busca la procedencia del número por su prefijo y reformatea la manera en
+    la cual se entienda mejor.
+     PARAMETROS:
+                num= número de telefono que introducimos a la función.
+                separar= separamos el número y el prefijo.
+                telefono= este es el telefono reformateado
+    Salida= Obtenemos el telefono y el país del que proviene"""
     separar = num[1:].split(")")
     pais = countries_dict[separar[0]]
     telefono = "+" + separar[0] + "-" + separar[1].replace("-", "")
 
     return telefono, pais
 def calculate_bill(multas_radar, multas_ITV, multas_estupefacientes):
+    """Esta funcion calcula el total de las mutlas que se introduzcan en los parametros.
+    PARAMETROS:
+                multas_radar= Son las multas que tenga el usuario por un radar
+                multas_itv= Son las multas que tenga el usuario por la ITV
+                multas_estupefacientes= Son el total de multas que tiene el usuario por estupefacientes"""
     bill = multas_radar + multas_ITV + multas_estupefacientes
     return bill
 
 
-def check_DGT():
-    """Esta funcion recoje un excel """
+def check_DGT(ruta):
+    """Esta funcion recoje un excel con diferentes parametros y reesctibe todos ellos corrigiendo los diferentes
+    parametros y reorganizando las celdas.
+    PARAMETROS:
+                ruta= Nombre del archivo el cual queremos modificar.
+    Salida: se creará un archivo en la carpeta que tenga guardado esta función, el cual tendrá todos los datos corregidos.
+    """
 
-    with open("Sergio Zabalegui Nicuesa - DGT.csv", encoding="utf-8") as csvfile:
+    with open(ruta, encoding="utf-8") as csvfile:
         reader = csv.reader(csvfile)
 
 
@@ -60,7 +82,7 @@ def check_DGT():
 
         excel_escritor = csv.writer(csvfile)
 
-    with open("Sergio Zabalegui Nicuesa - DGT.csv", encoding="utf-8") as salida:
+    with open(ruta, encoding="utf-8") as salida:
         csvsalida = open('salida.csv', 'w', newline='')
         salida = csv.writer(csvsalida,
                             quotechar='"',
@@ -70,6 +92,6 @@ def check_DGT():
     return
 
 
-
-check_DGT()
+ruta = input("Introduzca el nombre del archivo que quiere modificar")
+check_DGT(ruta)
 
